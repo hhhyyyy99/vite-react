0fcce169e7b365404335dc322205d683dd2df27a
Eamon <1025942836@qq.com>
2024-08-15 18:27:12 +0800

    feat: test

--- src/pages/testPage/index.tsx
+++ src/pages/testPage/index.tsx
@@ -1,12 +1,35 @@
-import { useEffect } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Header } from '../../components/Header';
+import { extractLinks,Verify} from '@eamon98/autolinker';
+function openLink(url: string) {
+  console.log(url);
+}
+function handleUrl(text: string) {
+ const result = extractLinks(text)
+  return result.map((item, index) => {
+   if(Verify.isUrl(item)){
+    return <a key={index} href={item} onClick={() => openLink(item)}>{item}</a>;
+   }
+   return <p key={index}>{item}</p>;
+  });
+}
 export default function Test() {
-  useEffect(()=>{
-  },[])
+  const [text, setText] = useState(
+    'www.baidu.com好家伙，这aaa127.0.0.1:9000hgdhjagdjw是一个测试http://192.168.0.1:9000啊大大大127.0.0.1',
+  );
+  const Element = useMemo(() => handleUrl(text), [text]);
   return (
     <div className="home-container">
       <Header />
       <div className="home-content">
+        <input
+          type="text"
+          placeholder="请输入内容"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {/* <div dangerouslySetInnerHTML={{ __html: Element()}}></div> */}
+        <div className="flex gap-2">{Element}</div>
       </div>
     </div>
   );
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,4 +1,3 @@
-import types from '@doaction/types';
 import * as radash from 'radash';
 
 export const data = {
@@ -39,19 +38,30 @@ interface DeepObject {
   [key: string]: DeepObject | object | string | null | number;
 }
 
-export function replaceFields<T extends DeepObject>(obj: T, fields: string[]): T {
-  Object.keys(obj).forEach((key: keyof T) => {
-    // @ts-ignore
-    if (fields.includes(key)) {
-      // @ts-ignore
-      obj[key] = 'xxx'; // 在这里，我们使用类型断言来确保 newValue 能被赋值给 obj[key]
-    } else if (
-      (types.isArray(obj[key]) || types.isObject(obj[key])) &&
-      (radash.isArray(obj[key]) || radash.isObject(obj[key]))
-    ) {
-      // @ts-ignore
-      replaceFields(obj[key] as T[keyof T], fields); // 注意这里必须进行类型断言，因为 T[keyof T] 可能不是一个对象
-    }
+// export function replaceFields<T extends DeepObject>(obj: T, fields: string[]): T {
+//   Object.keys(obj).forEach((key: keyof T) => {
+//     // @ts-ignore
+//     if (fields.includes(key)) {
+//       // @ts-ignore
+//       obj[key] = 'xxx'; // 在这里，我们使用类型断言来确保 newValue 能被赋值给 obj[key]
+//     } else if (
+//       (types.isArray(obj[key]) || types.isObject(obj[key])) &&
+//       (radash.isArray(obj[key]) || radash.isObject(obj[key]))
+//     ) {
+//       // @ts-ignore
+//       replaceFields(obj[key] as T[keyof T], fields); // 注意这里必须进行类型断言，因为 T[keyof T] 可能不是一个对象
+//     }
+//   });
+//   return obj;
+// }
+
+export function unique<T, K extends keyof T>(arr: T[], key: K | K[]): T[] {
+  const map = new Map<string, T>();
+  const result = arr.filter((item) => {
+    const keys = Array.isArray(key) ? key.map((k) => item[k]) : [item[key]];
+    const uniqueKey = keys.filter(Boolean).join('|'); // 如果有多个键，将它们连接成一个字符串
+    return !map.has(uniqueKey) && map.set(uniqueKey, item);
   });
-  return obj;
+  console.log({ map, result });
+  return result;
 }
